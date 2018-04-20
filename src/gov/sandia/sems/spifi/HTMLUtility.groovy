b58a5c46da1f8b6bac8570dbba9c264e1e4a1d93
William McLendon <wcmclen@sandia.gov>
2018-04-19 18:55:55 -0600

    Refactor of functions out of EmailMessage

--- src/gov/sandia/sems/spifi/HTMLUtility.groovy
+++ src/gov/sandia/sems/spifi/HTMLUtility.groovy
@@ -1,21 +1,16 @@
 #!/usr/bin/env groovy
 /**
- * EmailMessage.groovy
+ * HTMLUtility.groovy
  *
- * An email builder helper class that comes with some pre-configured 'canned'
- * emails as well as a customizable email setting.  This class provides a
- * HTML style pre-configured, allowing the point-of-use code to only need to
- * generate the appropriate email body.
+ * A set of routines to help with formatting HTML messages.  Provides a set of
+ * style rules and a basic template.
  *
  * @author  William McLendon
  * @version 1.0
- * @since   2018-04-04
+ * @since   2018-04-19
  *
  */
 package gov.sandia.sems.spifi;
-
-import gov.sandia.sems.spifi.HTMLUtility;
-
 
 
 
@@ -25,104 +20,49 @@ import gov.sandia.sems.spifi.HTMLUtility;
  * canned emails for the different status messages that we will generate in this
  * pipeline job.
  *
- * Canned email types are set by the emailType parameter.  Currently there are
- * the following types:
- * - SUCCESS : Canned email for SUCCESS status.
- * - FAILURE : Canned email for FAILURE status.
- * - CUSTOM  : Customizable email address.
- *
  */
-class EmailMessage implements Serializable
+class HTMLUtility implements Serializable
 {
     // Member Variables
     private static _env           // Requird Jenkins environment.
-    private String _emailBody     // (optional), if emailType is Custom, then use this.
-    private String _recipients    // comma-separated list of email address "person1@foo.com, person2@foo.com"
-    private String _replyTo       // Who should responses go to?
-    private String _emailType     // Notification Type - { "SUCCESS", "FAILURE", "CUSTOM" etc. }
-    private String _emailSubject  // Email Title
 
 
     /**
-     * Constructor for EmailMessage
+     * Constructor
      *
-     * @param env        [REQUIRED] Object  - Jenkins environment (use 'this' from the Jenkins pipeline)
-     * @param emailType  [REQUIRED] String  - What type of email should be generated.
-     *                                        Allowed values are [ SUCCESS | FAILURE | CUSTOM ]
-     * @param recipients [REQUIRED] String  - Recipient list. Space-separated list of email
-     *                                        recipients: "one@foo.com two@foo.com"
-     * @param replyTo    [REQUIRED] String] - Reply-To email address. Example: "bar@foo.com"
+     * @param env  [REQUIRED] Object  - Jenkins environment (use 'this' from the Jenkins pipeline)
      *
      * @return nothing
      */
-    EmailMessage(env, String emailType, String recipients, String replyTo)
+    HTMLUtility(Map params)
     {
-        this._env          = env
-        this._emailType    = emailType
-        this._replyTo      = replyTo
-        this._recipients   = recipients
-
-        switch(emailType)
+        // Validate parameters
+        if(!params.containsKey("env"))
         {
-            case "FAILURE":
-                this._emailSubject = "[Jenkins] Test Failure!"
-                this._emailBody = "<P>TEST FAILURE</P>"
-                break
-            case "SUCCESS":
-                this._emailSubject = "[Jenkins] Test Success!"
-                this._emailBody = "<P>TEST SUCCESS</P>"
-                break
-            case "CUSTOM":
-                this._emailSubject = "Status"
-                this._emailType = "CUSTOM"
-                break
-            default:
-                throw new Exception("Invalid emailType parameter provided to SPiFI::EmailMessage constructor.")
-                break
+            throw new Exception("[SPiFI] Missing required parameter: 'env'")
         }
+        this._env = params.env
     }
 
 
     /**
-     * Set the subject and body of a custom email message.
+     * generate
      *
-     * @param subject [REQUIRED] String - The subject of the email.
-     * @param body    [REQUIRED] String - The body of the email.  This goes inside
-     *                                    the <body></body> tags of the HTML message.
+     * @param body [REQUIRED] String  - String containing the body of the HTML to generate.
+     *                                  This is the content that would be inside the <BODY></BODY> elements
+     *                                  in a HTML document.
      *
-     * @return nothing
+     * @return String containing the assembled HTML document with template and headers.
      */
-    def setCustomEmail(String subject, String body)
+    def generate(Map params)
     {
-        this._emailSubject = subject
-        this._emailBody = body
-    }
-
-
-    /**
-     * Sends the email.
-     *
-     * @return nothing
-     */
-    def send()
-    {
-        try
+        if(!params.containsKey("body"))
         {
-            def htmlUtil = new HTMLUtility(env: this._env)
-
-            String emailBody = htmlUtil.generate(body: this._emailBody)
-
-            this._env.emailext(body: "${emailBody}",        \
-                               compressLog: true,           \
-                               replyTo: this._replyTo,      \
-                               subject: this._emailSubject, \
-                               to: this._recipients,        \
-                               mimeType: 'text/html')
+            throw new Exception("[SPiFI] Missing required parameter: 'body'.")
         }
-        catch(e)
-        {
-            throw new Exception("[SPiFI] An Error occurred attempting to send an email!\n${e}")
-        }
+        String output = this._genTemplate()
+        output = output.replace("{{BODY}}", params.body)
+        return output
     }
 
 
@@ -289,7 +229,7 @@ class EmailMessage implements Serializable
             {{STYLESHEET}}
             </HEAD>
             <BODY>
-            {{EMAIL_BODY}}
+            {{BODY}}
             <P><span class="gray">--<BR/>View output on <A HREF="${this._env.BUILD_URL}">Jenkins</A>.</span></P>
             </BODY>
             </HTML>
@@ -298,5 +238,5 @@ class EmailMessage implements Serializable
         return template
     }
 
-} // class EmailMessage
+} // class HTMLUtility
 
