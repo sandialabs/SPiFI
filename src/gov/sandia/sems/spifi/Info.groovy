8f59e02c47ffe016684d05415465a1c2b2ad00ec
William McLendon <wcmclen@sandia.gov>
2018-06-06 11:04:42 -0600

    Adding Info.groovy helpers file

---
+++ src/gov/sandia/sems/spifi/Info.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+/**
+ * Info.groovy
+ *
+ * Information function to get current version number, etc.
+ *
+ * @author  William McLendon
+ * @version 1.0
+ * @since   2018-06-06
+ *
+ */
+package gov.sandia.sems.spifi;
+
+
+
+/**
+ * Return the current version of SPiFI
+ *
+ * @return String containing the version number
+ */
+static def version()
+{
+    String spifi_version = "__master__"
+    return spifi_version
+}
+
